474ca58f78f7080e4ef43306c458aa053db19d16
eimontheintamara <eimontheintamarapura@gmail.com>
2018-06-23 22:40:48 +0630

    adding space

--- ex24.py
+++ ex24.py
@@ -2,7 +2,7 @@ print("Let's practice everything.")
 print('You\'d need to know \'bout escapes with \\ that do:')
 print('\n newlines and \t tabs.')
 
-poem="""
+poem = """
 \tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
@@ -12,13 +12,13 @@ and requires an explanation
   """
 print(poem)
 
-five=10-2+3-6
+five = 10 - 2 + 3 - 6
 print(f"This should be five:{five}")
 def secret_formula(started):
-    jelly_beans=started * 500
-    jars=jelly_beans / 1000
-    crates=jars / 100
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
     return jelly_beans,jars,crates
-start_point=10000
+start_point = 10000
 beans,jars,crates=secret_formula(start_point)
 #remember that this is another way to format a string
