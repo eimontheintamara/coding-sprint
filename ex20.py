a7f3a6f2ad1dc2db724723933bd38efe153bb7d6
eimontheintamara <eimontheintamarapura@gmail.com>
2018-06-23 22:37:11 +0630

    adding space

--- ex20.py
+++ ex20.py
@@ -8,8 +8,8 @@ def print_all(f):
 def rewind(f):
     f.seek(0)
 
-def print_a_line(line_count,f):
-    print(line_count,f.readline())
+def print_a_line(line_count, f):
+    print(line_count, f.readline())
 
 current_file = open(input_file)
 
