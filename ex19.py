a9011abc4597549acd0cfbdf6de54e6d7d00ae87
eimontheintamara <eimontheintamarapura@gmail.com>
2018-06-25 22:03:14 +0630

    Function and variables

--- ex19.py
+++ ex19.py
@@ -1,5 +1,5 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
-    print("You hace %d cheeses!" % cheese_count)
+    print("You have %d cheeses!" % cheese_count)
     print("You have %d boxes of crackers!" % boxes_of_crackers)
     print("Man that's enough for a party!")
     print("Get a blanket.\n")
@@ -16,5 +16,5 @@ cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 print("We can even do math inside too:")
 cheese_and_crackers(10 + 20, 5+ 6)
 
-print("And we can bombine the two,variables and math:")
+print("And we can combine the two,variables and math:")
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
